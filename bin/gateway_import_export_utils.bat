fdcfbb6e81c0b42707b5480c417a608ddb4379e5
Ian Ward <ianmward@gmail.com>
2020-07-23 16:53:31 +1000

    first test

--- bin/gateway_import_export_utils.bat
+++ bin/gateway_import_export_utils.bat
@@ -1,14 +1,14 @@
 @echo OFF
 
-set CURRENT_DIR=%~dp0
+set CURRENT_DIR=%~dp0..
 
 set ACTION=%1
 SHIFT 
 
 :loop
 IF NOT "%1"=="" (
-    IF "%1"=="_api_name" (
-        SET PROJECT_NAME=%3
+    IF "%1"=="-api_name" (
+        SET PROJECT_NAME=%2
         SHIFT
     )
     IF "%1"=="-apigateway_url" (
@@ -34,10 +34,10 @@ goto import
 )
 
 :import 
-IF EXIST %CURRENT_DIR%/apis/%PROJECT_NAME%/ (
- powershell Compress-Archive -Path %CURRENT_DIR%/apis/%PROJECT_NAME%/* -DestinationPath %CURRENT_DIR%/%PROJECT_NAME%.zip -Force
- curl -i -X POST %GATEWAY_URL%/rest/apigateway/archive?overwrite=* -H "Content-Type: application/zip" -H "Accept:application/json" --data-binary @"%CURRENT_DIR%/%PROJECT_NAME%.zip" --user %GATEWAY_USERNAME%:%GATEWAY_PASSWORD%
- del "%CURRENT_DIR%/%PROJECT_NAME%.zip"
+IF EXIST %CURRENT_DIR%\apis\%PROJECT_NAME%\ (
+ powershell Compress-Archive -Path %CURRENT_DIR%\apis\%PROJECT_NAME%\* -DestinationPath %CURRENT_DIR%\%PROJECT_NAME%.zip -Force
+ curl -i -X POST %GATEWAY_URL%/rest/apigateway/archive?overwrite=* -H "Content-Type: application/zip" -H "Accept:application/json" --data-binary @"%CURRENT_DIR%\%PROJECT_NAME%.zip" --user %GATEWAY_USERNAME%:%GATEWAY_PASSWORD%
+ del "%CURRENT_DIR%\%PROJECT_NAME%.zip"
  goto :EOF
 ) ELSE (
   echo "API with name %PROJECT_NAME% does not exists"
@@ -45,10 +45,11 @@ IF EXIST %CURRENT_DIR%/apis/%PROJECT_NAME%/ (
 )
 
 :export
-IF EXIST %CURRENT_DIR%/apis/%PROJECT_NAME%/ (
- curl %GATEWAY_URL%/rest/apigateway/archive -d @"%CURRENT_DIR%\apis\%PROJECT_NAME%\export_payload.json" --output %CURRENT_DIR%/%PROJECT_NAME%.zip -u  Administrator:manage -H "x-HTTP-Method-Override: GET" -H "Content-Type:application/json"
- powershell Expand-Archive -Path %CURRENT_DIR%/%PROJECT_NAME%.zip -DestinationPath %CURRENT_DIR%/apis/%PROJECT_NAME% -Force
- del "%CURRENT_DIR%/%PROJECT_NAME%.zip"
+echo on
+IF EXIST %CURRENT_DIR%\apis\%PROJECT_NAME%\ (
+ curl %GATEWAY_URL%/rest/apigateway/archive -d @"%CURRENT_DIR%\apis\%PROJECT_NAME%\export_payload.json" --output %CURRENT_DIR%\%PROJECT_NAME%.zip -u  Administrator:manage -H "x-HTTP-Method-Override: GET" -H "Content-Type:application/json"
+ powershell Expand-Archive -Path %CURRENT_DIR%\%PROJECT_NAME%.zip -DestinationPath %CURRENT_DIR%\apis\%PROJECT_NAME% -Force
+ del "%CURRENT_DIR%\%PROJECT_NAME%.zip"
 goto :EOF
 ) ELSE (
   echo "API with name %PROJECT_NAME% does not exists"
