3b1e2a4c2883f739400cfcccaf9e33517dab792b
Ian Ward <ianmward@gmail.com>
2020-09-09 11:20:23 +1000

    added export bat

---
+++ export-staff-service.bat
@@ -0,0 +1,1 @@
+.\bin\gateway_import_export_utils.bat export -apigateway_url https://api-gateway-development.aks.au-poc.com -apigateway_username Administrator -apigateway_password manage -api_name staff-service staff-service
